b7e921f1b67dff8e9ad64d126a65c11710240c24
Pranav Arora <89612594+AroraPranav@users.noreply.github.com>
2022-03-30 20:35:37 -0700

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
-@app.route("/")
+@app.route("/", methods =["GET", "POST"])
 def home():
     name = 'Lisa'
     city_names = ['Paris', 'London', 'Rome', 'Tahiti']
-    return render_template('home.html', title='home', len = len(city_names), name = name, city_names=city_names)
+    # if request.method == "POST":
+    #     name = request.form.get("cname")
+    #     return f'''<li>{name}</li>''' 
+    return render_template('home.html', title='home', len = len(city_names), name = name, city_names=city_names, added = request.form.get("cname"))
